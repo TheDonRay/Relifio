aa8bcf56fd7cce42f0707b9086b93abfa197d1c1
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-11-15 22:31:32 -0500

    route mounted on

--- backend/routes/signedup.js
+++ backend/routes/signedup.js
@@ -7,12 +7,12 @@ signupRoute.post('/signup', async (req, res) => {
         //TODO: implement the sign up page for users to be able to sign up for relief.io and send it to the mongo database 
         //for now we can have this: 
         const TemPData = { 
-            tempData: "set up database and data to be pushed into the database upon user signing up"
+            message: "set up database and data to be pushed into the database upon user signing up"
         }; 
-        const result = await TemPData;  
-        res.send(result); 
+        res.status(200).send(TemPData);  
 
     } catch (error){ 
         console.status(500).send('Error with sending Data to the database', error); 
     }
-})
+}); 
+
--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,22 @@ const express = require('express');
 const app = express(); 
 require('dotenv').config();  
 
-//import routes here as such 
+//import routes here as such  
+const signupRoute = require("./routes/signedup.js"); 
 
 // set up middleware 
 app.use(express.json()); 
 
 const PORT = process.env.PORT;  
-
+ 
 app.get('/', (req, res) => {
     res.json({ 
         BackendResponse: "server is running"
     }); 
-}); 
+});  
+
+//mount route here to the expressApp as such 
+app.use('/api/', signupRoute); 
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
