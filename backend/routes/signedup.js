4ec6fe58cd033d7eed4a1d054e6e3c3dab108f5a
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-11-27 13:00:02 -0500

    logic behind puhsing data into the model

--- backend/routes/signedup.js
+++ backend/routes/signedup.js
@@ -4,9 +4,26 @@ const signupRoute = express.Router();
 const signupSchema = require("../database/signupSchema.js");  
 
 // initialize the route here to be a post route.  
-signupRoute.post('/signup', (req, res) => { 
-
-})
+signupRoute.post('/signup', async (req, res) => { 
+  //The route is async because we are sending data to the frontend.  
+  // from the frontend we will get the text from the sign up box 
+  // set up a try and catch case to handle the input 
+  try {  
+    const { email } = req.body;  
+    const newSignup = signupSchema.create({ email }); 
+    res.status(200).json({ 
+      success: "user has Signed up details sent to database", 
+      data: newSignup, 
+    });  
+    console.log(newSignup); // for debugging purposes. 
+  } catch (error) { 
+      console.error('Error sending to the cluster in mongoDB', error); 
+      res.status(500).json({ 
+        message: error, 
+        isSuccessful: false,
+      }); 
+  }
+}); 
 
 
 module.exports = signupRoute;
