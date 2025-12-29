fa92d3eacb40a584a2a3a092163f075bfc18b5ba
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-12-29 13:00:33 -0500

    working on the rewording

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -91,7 +91,7 @@ export default function Home() {
             <span>Learn More</span>
           </button>
           <button className="btn2" onClick={buttonNav2}>
-            <span>Let's begin a chapter!</span>
+            <span>Begin a chapter!</span>
           </button>
         </div>
       </div>
