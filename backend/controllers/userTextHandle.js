490a46e080cb135b1f016720ad57eda9ec0cb438
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-12-30 14:31:52 -0500

    some backend logic added

---
+++ backend/controllers/userTextHandle.js
@@ -0,0 +1,17 @@
+//TODO:  May need to call the database model but we can modify that a bit letter to add it here 
+const userChapterHandling = async (req, res) => { 
+    // implement a try and catch case here 
+    try { 
+        const{ message }  = req.body;    
+        res.status(200).json({ 
+            messageRecieved: message
+        });; 
+    } catch (error) { 
+        console.error("Backend did not get a response", error);
+        res.json({ 
+            ErrorMessage: error
+        }); 
+    }
+}; 
+
+module.exports = userChapterHandling; 
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,5 +1,8 @@
 //External API route here  
-const express = require('express'); 
-const externalAPI = express.Router(); 
+const express = require('express');  
+const externalAPI = express.Router();   
+const userChapterHandling = require("../controllers/userTextHandle.js")
+
+externalAPI.post('/usertext', userChapterHandling); 
 
 module.exports = externalAPI; 
